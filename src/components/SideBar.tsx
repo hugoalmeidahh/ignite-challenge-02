b7af352a5a22629865cde9b825e8042e3b9bb41a
Hugo Almeida <hugoalmeidahh@gmail.com>
2021-12-30 19:19:20 -0300

    [001]: Reorganize structure project, Finish Challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+import { SideBarProps, GenreResponseProps } from '../types/allTypes'
+import { Button } from "./Button";
+import { api } from '../services/api';
+import '../styles/sidebar.scss';
+
+
+export function SideBar( {handleSetSelectedGenre, selectedGenreId}: SideBarProps) {
   // Complete aqui
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleSelectedGenre(id: number) {
+    handleSetSelectedGenre(id);
+  }
+
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleSelectedGenre(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
 }
---
+++ src/types/allTypes.ts
@@ -0,0 +1,25 @@
+export interface SideBarProps{
+  handleSetSelectedGenre:(id: number) => void;
+  selectedGenreId: number;
+}
+  
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export interface ContentProps{
+  selectedGenreId: number;
+}
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
